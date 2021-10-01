a69fa6b554f8e9dff7cb82dd9ffb72784c1d605f
Juan J. Rojas <juanjorojash@gmail.com>
2021-10-01 13:49:12 -0600

    new plotte

--- soc_test/plotter.py
+++ soc_test/plotter.py
@@ -1,23 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 ds = pd.read_csv('C:/Repositories/battery_characterizer/soc_test/discharge_data29_09_2021_19_13.csv')
 
-batt18650 = ds#[['time','seconds','voltage','current','capacity','temperature']]
-batt18650.columns = ['Time','Seconds (s)','Voltage (V)','Current (A)','Capacity (mAh)','Temperature (C)']
-#print(batt18650.describe())
+
+ds.columns = ['time','seconds','voltage','current','capacity','temperature']
 
 
+soc = ds.capacity/ds.capacity.max()
+print(soc)
 
+print(ds.seconds)
 
-fig = px.scatter(batt18650, x="Capacity (mAh)", y="Voltage (V)")
+ds = ds.assign(soc=soc.values)
+print(ds.describe())
+fig = px.scatter(ds, x="soc", y="voltage")
 fig.show()
-
-
-# plt.figure(figsize=[10,5])
-# plt.scatter(batt18650.time, batt18650.voltage, label='voltage')
-# plt.xlabel('time(s)', fontsize=15)
-# plt.ylabel('voltage(V)', fontsize=15)
-# plt.legend()
-# plt.show()
